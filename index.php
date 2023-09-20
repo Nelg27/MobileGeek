6ce5d83275ab95dcf2d7665f2ebfcf35c80b6b52
Nelg27 <Nassimelguennich27@gmail.com>
2023-09-19 22:50:17 -0400

    first commit - committing all the files to the repositery

---
+++ index.php
@@ -0,0 +1,190 @@
+<?php
+require "config/constants.php";
+session_start();
+if(isset($_SESSION["uid"])){
+	header("location:profile.php");
+}
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Mobilegeek</title>
+		<link rel="stylesheet" href="css/bootstrap.min.css"/>
+		<script src="js/jquery2.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+		<script src="main.js"></script>
+		<link rel="stylesheet" type="text/css" href="style.css">
+		<style></style>
+	</head>
+<body>
+<div class="wait overlay">
+	<div class="loader"></div>
+</div>
+	<div class="navbar navbar-inverse navbar-fixed-top">
+		<div class="container-fluid">	
+			<div class="navbar-header">
+				
+				<a href="#" class="navbar-brand">Mobilegeek</a>
+			</div>
+		<div class="collapse navbar-collapse" id="collapse">
+			<ul class="nav navbar-nav">
+				<li><a href="index.php"><span class="glyphicon glyphicon-home"></span>Home</a></li>
+				<li><a href="customer_registration.php?register=1"></span>S'inscrire</a></li>
+			</ul>
+		
+			<ul class="nav navbar-nav navbar-right">
+				<li><a href="#" class="dropdown-toggle" data-toggle="dropdown"></span>panier <span class="badge">0</span></a>
+					<div class="dropdown-menu" style="width:400px;">
+						<div class="panel panel-success">
+							<div class="panel-heading">
+								<div class="row">
+									<div class="col-md-3">Numéro</div>
+									<div class="col-md-3">Produit</div>
+									<div class="col-md-3">Nom</div>
+									<div class="col-md-3">Prix</div>
+								</div>
+							</div>
+							<div class="panel-body">
+								<div id="cart_product">
+								<!--<div class="row">
+									<div class="col-md-3">Sl.No</div>
+									<div class="col-md-3">Product Image</div>
+									<div class="col-md-3">Product Name</div>
+									<div class="col-md-3">Price in $.</div>
+								</div>-->
+								</div>
+							</div>
+							<div class="panel-footer"></div>
+						</div>
+					</div>
+				</li>
+				<li><a href="#" class="dropdown-toggle" data-toggle="dropdown">Se connecter</a>
+					<ul class="dropdown-menu">
+						<div style="width:300px;">
+							
+								<div class="panel-heading"><b>Identification </b></div>
+								<div class="panel-heading">
+									<form onsubmit="return false" id="login">
+										<label for="email">Email</label>
+										<input type="email" class="form-control" name="email" id="email" required/>
+										<label for="email">Mot de passe</label>
+										<input type="password" class="form-control" name="password" id="password" required/>
+										<p><br/></p>
+										<input type="submit"  style="float:right;" value="Se connecter">
+									</form>
+								</div>
+								
+							
+						</div>
+					</ul>
+				</li>
+			</ul>
+		</div>
+	</div>
+</div>	
+	<p><br/></p>
+	<p><br/></p>
+	<p><br/></p>
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-md-1"></div>
+			<div class="col-md-2 col-xs-12">
+				<div id="get_category">
+				</div>
+				<!--<div class="nav nav-pills nav-stacked">
+					<li class="active"><a href="#"><h4>Categories</h4></a></li>
+					<li><a href="#">Categories</a></li>
+					<li><a href="#">Categories</a></li>
+					<li><a href="#">Categories</a></li>
+					<li><a href="#">Categories</a></li>
+				</div> -->
+				<div id="get_brand">
+				</div>
+				<!--<div class="nav nav-pills nav-stacked">
+					<li class="active"><a href="#"><h4>Brand</h4></a></li>
+					<li><a href="#">Categories</a></li>
+					<li><a href="#">Categories</a></li>
+					<li><a href="#">Categories</a></li>
+					<li><a href="#">Categories</a></li>
+				</div> -->
+			</div>
+			<div class="col-md-8 col-xs-12">
+				<div class="row">
+					<div class="col-md-12 col-xs-12" id="product_msg">
+					</div>
+				</div>
+				<div class="panel panel-info">
+					<div class="panel-heading">Produits</div>
+					<div class="panel-body">
+						<div id="get_product">
+							<!--Here we get product jquery Ajax Request-->
+						</div>
+						<!--<div class="col-md-4">
+							<div class="panel panel-info">
+								<div class="panel-heading">Samsung Galaxy</div>
+								<div class="panel-body">
+									<img src="product_images/images.JPG"/>
+								</div>
+								<div class="panel-heading">$.500.00
+									<button style="float:right;" class="btn btn-danger btn-xs">AddToCart</button>
+								</div>
+							</div>
+						</div> -->
+					</div>
+					
+				</div>
+			</div>
+			<div class="col-md-1"></div>
+		</div>
+	</div>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
